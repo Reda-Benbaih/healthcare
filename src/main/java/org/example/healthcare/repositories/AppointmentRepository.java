12d67092fef441157f2120150cd05e6612ed6d2e
Reda Benbaih <wareda145@gmail.com>
2026-04-26 17:03:21 +0000

    repository finished

---
+++ src/main/java/org/example/healthcare/repositories/AppointmentRepository.java
@@ -0,0 +1,14 @@
+package org.example.healthcare.repositories;
+
+import org.example.healthcare.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment,Integer> {
+    List<Appointment> findByPatientId(Integer id);
+
+    List<Appointment> findByDoctorID(Integer id);
+}
---
+++ src/main/java/org/example/healthcare/repositories/PatientRepository.java
@@ -0,0 +1,10 @@
+package org.example.healthcare.repositories;
+
+import org.example.healthcare.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient,Integer> {
+
+}
