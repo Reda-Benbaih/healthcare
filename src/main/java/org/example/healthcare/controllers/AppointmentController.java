f5ccc7a304cd9abcf0d94e34c950d442cd1806b2
Reda-benbaih <wareda145@gmail.com>
2026-04-27 12:31:55 +0200

    finished Appointment Controller

--- src/main/java/org/example/healthcare/controllers/AppointmentController.java
+++ src/main/java/org/example/healthcare/controllers/AppointmentController.java
@@ -1,4 +1,47 @@
 package org.example.healthcare.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.example.healthcare.DTO.request.AppointmentRequestDTO;
+import org.example.healthcare.DTO.response.AppointmentResponseDTO;
+import org.example.healthcare.services.AppointmentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/{id}")
+@RequiredArgsConstructor
 public class AppointmentController {
+    private final AppointmentService appointmentService;
+
+    @PostMapping
+    public AppointmentResponseDTO createAppointment(@RequestBody AppointmentRequestDTO appointmentRequestDTO){
+        return appointmentService.createAppointment(appointmentRequestDTO);
+    }
+
+    @PutMapping("/{id}")
+    public AppointmentResponseDTO updateAppointment(@PathVariable Integer id , @RequestBody AppointmentRequestDTO appointmentRequestDTO){
+        return appointmentService.updateAppointment(id, appointmentRequestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAppointment(Integer id){
+        appointmentService.deleteAppointment(id);
+    }
+
+    @GetMapping
+    public List<AppointmentResponseDTO> showAllAppointment(){
+        return  appointmentService.showAllAppointment();
+    }
+
+    @GetMapping("/{id}")
+    public List<AppointmentResponseDTO> showAllAppointmentByPatientId(@PathVariable Integer id){
+        return appointmentService.showAllAppointmentByPatientId(id);
+    }
+
+    @GetMapping("/{id}")
+    public List<AppointmentResponseDTO> showAllAppointmentByDoctorId(@PathVariable Integer id){
+        return appointmentService.showAllAppointmentByDoctorId(id);
+    }
 }
