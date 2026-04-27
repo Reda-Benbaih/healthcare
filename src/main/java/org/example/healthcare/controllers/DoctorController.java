a1e9bc14305f135c7878b9424a989f039fc09d23
Reda-benbaih <wareda145@gmail.com>
2026-04-27 11:57:25 +0200

    finished Medical File

--- src/main/java/org/example/healthcare/controllers/DoctorController.java
+++ src/main/java/org/example/healthcare/controllers/DoctorController.java
@@ -1,4 +1,38 @@
 package org.example.healthcare.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.example.healthcare.DTO.request.DoctorRequestDTO;
+import org.example.healthcare.DTO.response.DoctorResponseDTO;
+import org.example.healthcare.services.DoctorService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/doctor")
+@RequiredArgsConstructor
 public class DoctorController {
+    private final DoctorService doctorService;
+
+    @PostMapping
+    public DoctorResponseDTO addDoctor(@RequestBody DoctorRequestDTO doctorRequestDTO){
+        return doctorService.addDoctor(doctorRequestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDoctor(@PathVariable Integer id){
+        doctorService.deleteDoctor(id);
+    }
+
+    @GetMapping
+    public List<DoctorResponseDTO> showAllDoctors(){
+        return doctorService.showAllDoctors();
+    }
+
+    @PutMapping("/{id}")
+    public DoctorResponseDTO updateDoctor(@PathVariable Integer id ,@RequestBody DoctorRequestDTO doctorRequestDTO){
+        return  doctorService.updateDoctor(id, doctorRequestDTO);
+    }
+
 }
--- src/main/java/org/example/healthcare/controllers/MedicalFileController.java
+++ src/main/java/org/example/healthcare/controllers/MedicalFileController.java
@@ -1,4 +1,30 @@
 package org.example.healthcare.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.example.healthcare.DTO.request.MedicalFileRequestDTO;
+import org.example.healthcare.DTO.response.MedicalFileResponseDTO;
+import org.example.healthcare.services.MedicalFileService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/api/medicalFile}")
+@RequiredArgsConstructor
 public class MedicalFileController {
+    private final MedicalFileService medicalFileService;
+
+    @PostMapping
+    public MedicalFileResponseDTO addMedicalFile(@RequestBody MedicalFileRequestDTO medicalFileRequestDTO){
+        return medicalFileService.addMedicalFile(medicalFileRequestDTO);
+    }
+
+    @GetMapping("/{id}")
+    public MedicalFileResponseDTO showMedicalFileById(@PathVariable Integer id){
+        return medicalFileService.showMedicalFileById(id);
+    }
+
+    @PutMapping("/{id]")
+    public MedicalFileResponseDTO updateMedicalFile(@PathVariable Integer id, @RequestBody MedicalFileRequestDTO medicalFileRequestDTO){
+        return medicalFileService.updateMedicalFile(id, medicalFileRequestDTO);
+    }
 }
