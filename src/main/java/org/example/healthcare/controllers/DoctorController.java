461d6478f35bf1c19f6fe5e1ab3e8b19532745cb
Reda-benbaih <wareda145@gmail.com>
2026-04-27 11:15:04 +0200

    finished patient Controller

---
+++ src/main/java/org/example/healthcare/controllers/DoctorController.java
@@ -0,0 +1,4 @@
+package org.example.healthcare.controllers;
+
+public class DoctorController {
+}
---
+++ src/main/java/org/example/healthcare/controllers/PatientController.java
@@ -0,0 +1,43 @@
+package org.example.healthcare.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.example.healthcare.DTO.request.PatientRequestDTO;
+import org.example.healthcare.DTO.response.PatientResponseDTO;
+import org.example.healthcare.services.PatientService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/patient")
+@RequiredArgsConstructor
+public class PatientController {
+    private final PatientService patientService;
+
+    @PostMapping
+    public PatientResponseDTO addPatient(@RequestBody PatientRequestDTO patientRequestDTO){
+        return patientService.addPatient(patientRequestDTO);
+    }
+
+    @GetMapping
+    public List<PatientResponseDTO> showAllPatient(){
+        return patientService.showAllPatients();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePatient(@PathVariable Integer id){
+        patientService.deletePatient(id);
+    }
+
+    @GetMapping("/{id}")
+    public PatientResponseDTO showPatientById(@PathVariable Integer id){
+        return patientService.showPatientById(id);
+    }
+
+    @PutMapping("/{id}")
+    public PatientResponseDTO updatePatient(@PathVariable Integer id,@RequestBody PatientRequestDTO patientRequestDTO){
+        return patientService.updatePatient(id,patientRequestDTO);
+    }
+
+}
