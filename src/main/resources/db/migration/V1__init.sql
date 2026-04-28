65ad53a77f04a957f6ef526421349cc457960946
Reda-benbaih <wareda145@gmail.com>
2026-04-28 13:39:44 +0200

    all diagramme added

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,11 +1,9 @@
-create table appointment(
+create table patient(
     id int auto_increment primary key,
-    appointment_date DATE NOT NULL,
-    status VARCHAR(50) not null,
-    doctor_id INT NOT NULL,
-    patient_id INT NOT NULL,
-    CONSTRAINT fk_appointment_doctor FOREIGN KEY (doctor_id) REFERENCES doctor(id),
-    CONSTRAINT fk_appointment_patient FOREIGN KEY (patient_id) REFERENCES patient(id)
+    first_name varchar(255) not null ,
+    last_name varchar(255) not null,
+    email varchar(255) not null unique ,
+    phone bigint not null unique
 );
 create table doctor(
     id int auto_increment primary key,
@@ -14,12 +12,14 @@ create table doctor(
     email varchar(255) not null unique ,
     phone bigint not null unique
 );
-create table patient(
+create table appointment(
     id int auto_increment primary key,
-    first_name varchar(255) not null ,
-    last_name varchar(255) not null,
-    email varchar(255) not null ,
-    phone bigint not null unique
+    appointment_date DATE NOT NULL,
+    status VARCHAR(50) not null,
+    doctor_id INT NOT NULL,
+    patient_id INT NOT NULL,
+    CONSTRAINT fk_appointment_doctor FOREIGN KEY (doctor_id) REFERENCES doctor(id) ON DELETE CASCADE,
+    CONSTRAINT fk_appointment_patient FOREIGN KEY (patient_id) REFERENCES patient(id) ON DELETE CASCADE
 );
 create table medical_file(
     id int auto_increment primary key,
@@ -27,5 +27,5 @@ create table medical_file(
     observation varchar(1024) not null ,
     creation_date date not null,
     patient_id INT NOT NULL,
-    CONSTRAINT fk_medical_file_patient FOREIGN KEY (patient_id) REFERENCES patient(id)
+    CONSTRAINT fk_medical_file_patient FOREIGN KEY (patient_id) REFERENCES patient(id) ON DELETE CASCADE
 )
